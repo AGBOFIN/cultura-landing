4598f5916342fd0268a8413837c5892211cdda5a
AGBOFIN <agbofinmoses07@gmail.com>
2026-07-15 12:23:39 +0000

    Initial commit: CULTIVA landing page with Next.js 15, React 19, Tailwind CSS, Framer Motion

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: [],
   },
-  eslint: {
-    // Ajout de cette option pour ignorer les erreurs ESLint lors du build sur Vercel
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
